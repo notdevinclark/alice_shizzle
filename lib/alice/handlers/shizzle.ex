efc3e63f9a505f39867aadc643404e9cb0ffa3c0
Devin Clark <notdevinclark@gmail.com>
2016-03-02 17:21:26 -0600

    updates n' refactors tha shizzle handlez wit tha url creator function querizzle

--- lib/alice/handlers/shizzle.ex
+++ lib/alice/handlers/shizzle.ex
@@ -1,14 +1,25 @@
 defmodule Alice.Handlers.Shizzle do
   use Alice.Router
 
-  command ~r/(shizzle)\s+(me )?(?<term>.+)/i, :tranzizzle
-  route   ~r/(shizzle)\s+(me )?(?<term>.+)/i, :tranzizzle
+  command ~r/\bshizzle\s+me (?<term>.+)/i, :tranzizzle
+  route   ~r/\Ashizzle\s+me (?<term>.+)/i, :tranzizzle
+  command ~r/\bshizzle\s+url (?<term>.+)/i, :querizzle
+  route   ~r/\Ashizzle\s+url (?<term>.+)/i, :querizzle
 
-  def handle(conn, :tranzizzle) do
+  def handle(conn, name) when name in [:tranzizzle, :querizzle] do
     conn
     |> extract_term
-    |> Gizoogle.translate
+    |> shizzlize(name)
     |> reply(conn)
+  end
+
+  defp shizzlize(text, :tranzizzle), do: text |> Gizoogle.translate
+  defp shizzlize(text, :querizzle) do
+    text |> santize_url |> Gizoogle.url
+  end
+
+  defp santize_url(url) do
+    url |> String.replace(~r/\A<([^|]+)(.*)?>\z/, "\\1")
   end
 
   defp extract_term(conn) do
