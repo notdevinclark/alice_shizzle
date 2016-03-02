349051504348322bd9e4b8caea8de9c560af5340
Devin Clark <notdevinclark@gmail.com>
2016-03-02 15:46:47 -0600

    bumps tha version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AliceShizzle.Mixfile do
 
   def project do
     [app: :alice_shizzle,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
