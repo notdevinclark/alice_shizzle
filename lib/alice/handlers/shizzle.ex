b75bd303652fb188a01c7b405d87456af4120bfb
Devin Clark <notdevinclark@gmail.com>
2016-03-09 19:08:36 -0600

    fixes slacks wack way of messin up tha lyrics I receive
    Slack chizzlez usernames, emails, urls, n' double n' single quotes so
    dat it is thugged-out ta slack. This causes problems when bustin
    thangs ta gizoogle n' retrievin dem back all up in tha xml. I have
    added sanitizers now dat properly handle tha weird thangs dat slack do
    so dat we can translate n' parse thangs properly n' then bust dem back
    ta slack safely.

--- lib/alice/handlers/shizzle.ex
+++ lib/alice/handlers/shizzle.ex
@@ -1,10 +1,6 @@
 defmodule Alice.Handlers.Shizzle do
   use Alice.Router
 
-  @slack_url_pattern ~r/\<([^|]+)(.*)?>/
-  @slack_email_pattern ~r/\<mailto:([^|]+)(.*)?>/
-  @slack_username_pattern ~r/\<@([^|]+)>/
-
   command ~r/\bshizzle\s+me (?<term>.+)/i, :tranzizzle
   route   ~r/\Ashizzle\s+me (?<term>.+)/i, :tranzizzle
   command ~r/\bshizzle\s+url (?<term>.+)/i, :querizzle
@@ -18,58 +14,44 @@ defmodule Alice.Handlers.Shizzle do
   @doc "`shizzle url ____` - link ta a tranzizzled url or search term"
   def querizzle(connizzle),  do: handizzle(connizzle, :querizzle)
 
-  def handizzle(connizzle, name) do
+  defp handizzle(connizzle, name) do
     connizzle
     |> extract_dat_term
-    |> santizzle(connizzle)
+    |> sanitizzle
     |> shizzlize(name)
+    |> unhide_yo_usernames
     |> reply(connizzle)
   end
 
-  def extract_dat_term(connizzle) do
+  defp extract_dat_term(connizzle) do
     connizzle.message.captures
     |> Enum.reverse
     |> hd
   end
 
-  def shizzlize(text, :tranzizzle), do: text |> Gizoogle.translate
-  def shizzlize(text, :querizzle),  do: text |> Gizoogle.url
+  defp shizzlize(text, :tranzizzle), do: text |> Gizoogle.translate
+  defp shizzlize(text, :querizzle),  do: text |> Gizoogle.url
 
-  def santizzle(text, connizzle) do
+  def sanitizzle(text) do
     text
-    |> santizzle_punctuations
-    |> santizzle_urls
-    |> santizzle_emails
-    # |> santizzle_usernames(connizzle)
+    |> hide_yo_punctuations
+    |> hide_yo_emails
+    |> hide_yo_usernames
+    |> hide_yo_urls
   end
 
-  def santizzle_punctuations(text) do
+  defp hide_yo_punctuations(text) do
     text
-    |> String.replace("“", "\"")
-    |> String.replace("”", "\"")
-    |> String.replace("’", "'")
+    |> String.replace("“", ~s("))
+    |> String.replace("”", ~s("))
+    |> String.replace("’", ~s('))
   end
 
-  def santizzle_urls(text), do: text |> String.replace(@slack_url_pattern, "\\1")
+  defp hide_yo_emails(text), do: text |> String.replace(~r/<mailto:([^|]+)[^\s]*>/i, "\\1")
 
-  def santizzle_emails(text), do: text |> String.replace(@slack_email_pattern, "\\1")
+  defp hide_yo_urls(text), do: text |> String.replace(~r/<([^|@]+)([^\s]*)?>/, "\\1")
 
-  # def santizzle_usernames(text, connizzle), do: text |> String.replace(@slack_username_pattern, retreive_username("\\1", connizzle))
+  defp hide_yo_usernames(text), do: text |> String.replace(~r/<(@[\w\d]+)>/, "#:#\\1#:#")
 
-  # def retreive_username(username_code, connizzle) do
-  #   connizzle.slack.users[username_code].name
-  # end
-
-  # def something(text, connizzle) do
-  #   ~r/<@(.*?)>/
-  #   |> Regex.scan(text)
-  #   |> Enum.uniq
-  #   |> something_else(text)
-  # end
-
-  # def something_else([], text)
-
-  # def something_else([[matcher, match]|tail], text) do
-    
-  # end
+  def unhide_yo_usernames(text), do: text |> String.replace(~r/#:#(@[\w\d]+)#:#/, "<\\1>")
 end
