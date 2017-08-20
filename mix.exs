573354110da4e44a83fc4ce2d98b495535127404
Devin Clark <notdevinclark@gmail.com>
2017-08-20 15:27:47 -0700

    updates version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AliceShizzle.Mixfile do
 
   def project do
     [app: :alice_shizzle,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
